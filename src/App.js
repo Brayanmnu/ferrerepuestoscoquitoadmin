6bbe5bba20f4eee527a7ea8431e836459bcebb71
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-08-19 10:06:54 -0500

    Se modifica login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route , Routes} from 'react-router-dom'
 import ProductDetails from './modules/ProductDetails'
-import Login from './modules/Login'
+import Dashboard from './modules/Dashboard'
+import ThemeLogin from './components/ThemeLogin'
 
 export default function App() {
-  const[isDetails, setIsDetails] = React.useState(true);
-  return (
+  const[isDetails, setIsDetails] = useState(true);
+  const[isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const loggedCoquitoJSON = window.localStorage.getItem('loggedCoquito')
+    if(loggedCoquitoJSON){
+        setIsLogin(true)
+    }else{
+        setIsLogin(false)
+    }
+  }, [isLogin]);
+
+
+  return isLogin?
     <Router>
       <Routes>
         <Route path={'/products-details/:idProduct'} exact element={<ProductDetails setIsDetails={setIsDetails}/>} />
       </Routes>
-        {isDetails? <Login/>:null}
+        {isDetails? <Dashboard setIsLogin={setIsLogin}/> :null}
     </Router>
-  );
+  :  <ThemeLogin setIsLogin={setIsLogin}/>;
 }
