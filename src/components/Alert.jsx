be360f0173b1d16a9f0e8ee34ab34f9f77e2e8da
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-08-22 08:33:52 -0500

    Se aumenta el tiempo de alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -8,7 +8,7 @@ export default function Alert(props) {
         props.setOpenAlert(false);
     };
     return(
-        <Snackbar open={props.openAlert} autoHideDuration={2000} onClose={handleCloseAlert}>
+        <Snackbar open={props.openAlert} autoHideDuration={5000} onClose={handleCloseAlert}>
             <MuiAlert onClose={handleCloseAlert} severity={props.severity} sx={{ width: '100%' }}>
                 {props.mensaje}
             </MuiAlert>
