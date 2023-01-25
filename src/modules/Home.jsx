cf46d3503801620c11ca5ed8765bcff9c9f1ea14
Brayanmnu <brayan.neyra@hotmail.com>
2023-01-25 09:34:52 -0500

    Se elimina bug

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Grid from '@mui/material/Grid';
-import Image from '../img/home2.jpg'; // Import using relative path
-import CssBaseline from '@mui/material/CssBaseline';
+
 
 export default function Home(props) {
 
